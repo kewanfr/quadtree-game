3c6ab4f18145a1bbc20a19498a096f2b361c2a84
Kéwan B <mail@kewan.fr>
2024-11-12 15:39:47 +0100

    Source Code

---
+++ floor/init.go
@@ -0,0 +1,28 @@
+package floor
+
+import (
+	"gitlab.univ-nantes.fr/jezequel-l/quadtree/configuration"
+	"gitlab.univ-nantes.fr/jezequel-l/quadtree/quadtree"
+)
+
+// Init initialise les structures de données internes de f.
+func (f *Floor) Init() {
+	f.content = make([][]int, configuration.Global.NumTileY)
+	for y := 0; y < len(f.content); y++ {
+		f.content[y] = make([]int, configuration.Global.NumTileX)
+	}
+
+	switch configuration.Global.FloorKind {
+	case FromFileFloor:
+		f.fullContent = readFloorFromFile(configuration.Global.FloorFile)
+	case QuadTreeFloor:
+		f.quadtreeContent = quadtree.MakeFromArray(readFloorFromFile(configuration.Global.FloorFile))
+	}
+}
+
+// lecture du contenu d'un fichier représentant un terrain
+// pour le stocker dans un tableau
+func readFloorFromFile(fileName string) (floorContent [][]int) {
+	// TODO
+	return
+}
