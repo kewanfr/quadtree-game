3e92d7ca9dc92a42842fe930337a0dd305db65d3
Kéwan B <mail@kewan.fr>
2024-11-12 16:33:51 +0100

    tests for readFloorFromFile

--- floor/init_test.go
+++ floor/init_test.go
@@ -1,20 +1,36 @@
 package floor
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
 
 func TestReadExemple(t *testing.T) {
-	var testArr = [][]int{{1, 1, 3, 4}, {1, 1, 4, 3}, {0, 0, 2, 2}, {0, 0, 2, 2}}
-	var result = readFloorFromFile("exemple")
+	var want = [][]int{{1, 1, 3, 4}, {1, 1, 4, 3}, {0, 0, 2, 2}, {0, 0, 2, 2}}
+	var result = readFloorFromFile("../floor-files/exemple")
 
 	if result == nil {
 		t.Error("Erreur: result is nil")
 	}
 
-	if !reflect.DeepEqual(result, testArr) {
-		t.Errorf("Erreur: expected %v, got %v", testArr, result)
+	if !reflect.DeepEqual(result, want) {
+		t.Errorf("Erreur: expected %v, got %v", want, result)
+	}
+}
+
+func TestReadEmpty(t *testing.T) {
+	var want [][]int = [][]int{}
+	var result = readFloorFromFile("../tests/emptyFile")
+
+	fmt.Println(result)
+
+	if result == nil {
+		t.Error("Erreur: result is nil")
+	}
+
+	if !reflect.DeepEqual(result, want) {
+		t.Errorf("Erreur: expected %v, got %v", want, result)
 	}
 }
 
