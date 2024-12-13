7f6b5f3d5c6d5ca44faf92bf7fce54e1160d666d
chianti-ga <chiantigally@gmail.cm>
2024-12-13 13:50:06 +0100

    première implémentation pour les quadtree, ne marche pas avec les rectangles != 2^n

--- quadtree/make.go
+++ quadtree/make.go
@@ -3,6 +3,66 @@ package quadtree
 // MakeFromArray construit un quadtree représentant un terrain
 // étant donné un tableau représentant ce terrain.
 func MakeFromArray(floorContent [][]int) (q Quadtree) {
-	// TODO
+	height := len(floorContent)
+	if height == 0 {
+		return Quadtree{}
+	}
+	width := len(floorContent[0])
+
+	q.width = width
+	q.height = height
+	q.root = makeNode(floorContent, 0, 0, width, height)
+
 	return
+}
+
+// func recursive pour faire les node
+func makeNode(floorContent [][]int, startX, startY, width, height int) *node {
+	if width == 1 && height == 1 {
+		return &node{
+			topLeftX: startX,
+			topLeftY: startY,
+			width:    1,
+			height:   1,
+			content:  floorContent[startY][startX],
+			isLeaf:   true,
+		}
+	}
+
+	var halfWidth int = width / 2
+	var halfHeight int = height / 2
+
+	var topLeftNode *node = makeNode(floorContent, startX, startY, halfWidth, halfHeight)
+	var topRightNode *node = makeNode(floorContent, startX+halfWidth, startY, width-halfWidth, halfHeight)
+	var bottomLeftNode *node = makeNode(floorContent, startX, startY+halfHeight, halfWidth, height-halfHeight)
+	var bottomRightNode *node = makeNode(floorContent, startX+halfWidth, startY+halfHeight, width-halfWidth, height-halfHeight)
+
+	if topLeftNode != nil && topRightNode != nil &&
+		bottomLeftNode != nil && bottomRightNode != nil &&
+		topLeftNode.isLeaf && topRightNode.isLeaf &&
+		bottomLeftNode.isLeaf && bottomRightNode.isLeaf &&
+		topLeftNode.content == topRightNode.content &&
+		topRightNode.content == bottomLeftNode.content &&
+		bottomLeftNode.content == bottomRightNode.content {
+		return &node{
+			topLeftX: startX,
+			topLeftY: startY,
+			width:    width,
+			height:   height,
+			content:  topLeftNode.content,
+			isLeaf:   true,
+		}
+	}
+
+	return &node{
+		topLeftX:        startX,
+		topLeftY:        startY,
+		width:           width,
+		height:          height,
+		isLeaf:          false,
+		topLeftNode:     topLeftNode,
+		topRightNode:    topRightNode,
+		bottomLeftNode:  bottomLeftNode,
+		bottomRightNode: bottomRightNode,
+	}
 }
