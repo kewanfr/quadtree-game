889d3cbc85f2be0960318cbb98dd2837178c6915
Kéwan B <mail@kewan.fr>
2024-12-17 10:39:50 +0100

    fonction contentAt

--- quadtree/get.go
+++ quadtree/get.go
@@ -1,4 +1,42 @@
 package quadtree
+func (n *node) contentAt(x, y int) int {
+	/*
+	A partir d'un node, renvoie le contenu aux coordonnées (x, y)
+	*/
+	// Si la node est nil, il n'y a pas de sous-quadtree, donc on renvoie -1
+	if n == nil {
+		return -1
+	if x < n.topLeftX || x >= n.topLeftX+n.width ||
+	   y < n.topLeftY || y >= n.topLeftY+n.height {
+		return -1
+	}
+	if n.isLeaf {
+		return n.content
+	}
+	halfW := n.width / 2
+	halfH := n.height / 2
+	splitX := n.topLeftX + halfW
+	splitY := n.topLeftY + halfH
+
+	if y < splitY {
+		if x < splitX {
+			// Partie Gauche
+			return n.topLeftNode.contentAt(x, y)
+		} else {
+			// Partie droite
+			return n.topRightNode.contentAt(x, y)
+		}
+	} else {
+		// Si on est dans la partie basse 
+		if x < splitX { 
+			// partie gauche
+			return n.bottomLeftNode.contentAt(x, y)
+		} else {
+			//partie droite
+			return n.bottomRightNode.contentAt(x, y)
+		}
+	}
+}
 
 // GetContent remplit le tableau contentHolder (qui représente
 // un terrain dont la case le plus en haut à gauche a pour coordonnées
