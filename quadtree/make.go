719ae95f181b569bff81c6aa4f127364501aff30
chianti-ga <chiantigally@gmail.cm>
2024-12-18 16:38:04 +0100

    better documentation and simplification in the reduction logic (removed the nil check since it should never happen)

--- quadtree/make.go
+++ quadtree/make.go
@@ -3,20 +3,46 @@ package quadtree
 // MakeFromArray construit un quadtree représentant un terrain
 // étant donné un tableau représentant ce terrain.
 func MakeFromArray(floorContent [][]int) (q Quadtree) {
-	height := len(floorContent)
+	height := len(floorContent) // y
 	if height == 0 {
 		return Quadtree{}
 	}
-	width := len(floorContent[0])
+	width := len(floorContent[0]) // x
 
 	q.width = width
 	q.height = height
+
+	// node parent
 	q.root = makeNode(floorContent, 0, 0, width, height)
 
 	return q
 }
 
+/*
+*
+
+	  	makeNode est une fonction récursive qui construit un nœud du quadtree.
+
+		Elle prend en entrée un tableau représentant le contenu du terrain,
+		les coordonnées du coin supérieur gauche de la zone à représenter,
+		ainsi que sa largeur et sa hauteur.
+
+		Elle s'arrête lorsqu'un node a pour dimension 1x1 contient une valeur à 0 dans ses dimensions.
+
+		Paramètres :
+		- floorContent : [][]int - Le contenu du terrain, représenté par un tableau 2D d'entiers.
+		- startX : int - La coordonnée X du coin supérieur gauche de la zone à représenter.
+		- startY : int - La coordonnée Y du coin supérieur gauche de la zone à représenter.
+		- width : int - La largeur de la zone à représenter.
+		- eight : int - La hauteur de la zone à représenter.
+
+		Sortie :
+		- *node : Un pointeur vers le nœud du quadtree créé.
+
+*
+*/
 func makeNode(floorContent [][]int, startX, startY, width, height int) *node {
+	// Arrêt de la fonction récursive si les diemension sont 1x1 ou contiennent 0
 	if width <= 0 || height <= 0 || (width == 1 && height == 1) {
 		return &node{
 			topLeftX: startX,
@@ -38,10 +64,8 @@ func makeNode(floorContent [][]int, startX, startY, width, height int) *node {
 	var bottomLeftNode *node = makeNode(floorContent, startX, startY+halfHeight, halfWidth, height-halfHeight)
 	var bottomRightNode *node = makeNode(floorContent, startX+halfWidth, startY+halfHeight, width-halfWidth, height-halfHeight)
 
-	//Réduction et elimination des doublons
-	if topLeftNode != nil && topRightNode != nil &&
-		bottomLeftNode != nil && bottomRightNode != nil &&
-		topLeftNode.isLeaf && topRightNode.isLeaf &&
+	// Réduction et elimination des doublons
+	if topLeftNode.isLeaf && topRightNode.isLeaf &&
 		bottomLeftNode.isLeaf && bottomRightNode.isLeaf &&
 		topLeftNode.content == topRightNode.content &&
 		topRightNode.content == bottomLeftNode.content &&
@@ -56,6 +80,7 @@ func makeNode(floorContent [][]int, startX, startY, width, height int) *node {
 		}
 	}
 
+	// On retourne le noeud
 	return &node{
 		topLeftX:        startX,
 		topLeftY:        startY,
