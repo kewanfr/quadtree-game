5e9452c7defcea80201b9d80502274f64f1df9d8
Kéwan B <mail@kewan.fr>
2025-01-13 17:23:18 +0100

    Extension: Enregistrement d'un terrain avec touche F5

--- floor/floor.go
+++ floor/floor.go
@@ -1,6 +1,10 @@
 package floor
 
 import (
+	"os"
+	"strconv"
+
+	"gitlab.univ-nantes.fr/jezequel-l/quadtree/configuration"
 	"gitlab.univ-nantes.fr/jezequel-l/quadtree/quadtree"
 )
 
@@ -50,4 +54,37 @@ func (f Floor) GetWidth() (width int) {
 		width = len(f.fullContent[0])
 	}
 	return
+}
+
+func (f Floor) SaveFloor() {
+
+	var floorContent [][]int = make([][]int, configuration.Global.NumTileX)
+	for i := range floorContent {
+		floorContent[i] = make([]int, configuration.Global.NumTileY)
+	}
+
+	topLeftX := 0
+	topLeftY := 0
+	f.quadtreeContent.GetContent(topLeftX, topLeftY, floorContent)
+
+	// fmt.Println("floorContent", floorContent)
+
+	file, err := os.Create("../floor-files/floor")
+
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(floorContent); i++ {
+		line := ""
+		for j := 0; j < len(floorContent[i]); j++ {
+			// fmt.Println(floorContent[i][j])
+			line += strconv.Itoa(floorContent[i][j])
+		}
+		// fmt.Println(line)
+		file.WriteString(line + "\n")
+
+	}
+
+	file.Close()
 }
