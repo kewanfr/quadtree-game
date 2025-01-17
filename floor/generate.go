5530138d13d2c5c5d84e127273124481ee73c513
Kéwan B <mail@kewan.fr>
2025-01-17 10:05:22 +0100

    ExtRandomFloorGeneration: fix seed for random ints

--- floor/generate.go
+++ floor/generate.go
@@ -1,14 +1,17 @@
 package floor
 
 import (
+	"math/rand"
+	"time"
+
 	"gitlab.univ-nantes.fr/jezequel-l/quadtree/configuration"
-	"golang.org/x/exp/rand"
 )
 
 func generateRandomFloorContent() (floorContent [][]int) {
 	for x := 0; x < configuration.Global.NumTileX; x++ {
 		var line []int
 		for y := 0; y < configuration.Global.NumTileY; y++ {
+			rand.Seed(time.Now().UnixNano())
 			line = append(line, rand.Intn(5))
 		}
 		floorContent = append(floorContent, line)
