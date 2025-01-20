565fd1d67e038fc690925d6844f770298e4f2ef9
chianti-ga <chiantigally@gmail.cm>
2025-01-20 10:25:36 +0100

    fix particles drawing

--- particles/draw.go
+++ particles/draw.go
@@ -15,16 +15,12 @@ Draw dessine les particules sur l'écran
 *
 */
 func (p Particle) Draw(screen *ebiten.Image, camX, camY int) {
-	xShift := 0
-	yShift := 0
-
-	// Calcule la position de la particule sur le terrain
+	// Calcule la position de l'animation sur le terrain
 	xTileForDisplay := p.X - camX + configuration.Global.ScreenCenterTileX
 	yTileForDisplay := p.Y - camY + configuration.Global.ScreenCenterTileY
 
-	// Calcule la position de la particule sur l'écran
-	xPos := (xTileForDisplay)*configuration.Global.TileSize + xShift
-	yPos := (yTileForDisplay)*configuration.Global.TileSize - configuration.Global.TileSize/2 + 2 + yShift
+	xPos := (xTileForDisplay) * configuration.Global.TileSize
+	yPos := (yTileForDisplay) * configuration.Global.TileSize
 
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(float64(xPos), float64(yPos))
